ac93b8a2a2681b134112a1d1c7536a68f63d75b5
VahideRamezani <vhiderm@gmail.com>
2025-05-01 15:56:32 +0330

    header

---
+++ project/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        yekan: ['yekan'], // اینو اینجا اضافه کن
+      },
+    },
+  },
+  plugins: [],
+}
+
