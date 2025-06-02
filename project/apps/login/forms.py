636d54e197186f7e9e92d45993c2fc9f907d80b2
VahideRamezani <vhiderm@gmail.com>
2025-06-02 20:02:25 +0330

    teacher login

--- project/apps/login/forms.py
+++ project/apps/login/forms.py
@@ -7,3 +7,11 @@ class SLoginForm(forms.Form):
         'placeholder':'کد ملی خود را به طور کامل وارد کنید' }))
     st_password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'w-full px-4 py-1 md:py-3 text-lg border border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white/90',
         'placeholder' : 'رمز عبور شما همان کد ملی شما می باشد'}))
+
+class TLoginForm(forms.Form):
+        teacher_code = forms.CharField(widget=forms.TextInput(attrs={
+            'class': 'col-span-9 px-4 py-2 md:py-3 text-lg border border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white/90',
+            'placeholder': 'کد پرسنلی خود را به طور کامل وارد کنید'}))
+        teacher_password = forms.CharField(widget=forms.PasswordInput(attrs={
+            'class': 'col-span-9 px-4 py-2 md:py-3 text-lg border border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white/90',
+            'placeholder': 'رمز عبور شما همان کد پرسنلی شما می باشد'}))
--- project/apps/login/views.py
+++ project/apps/login/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render, redirect
-from .forms import SLoginForm
-from .models import students
+from .forms import SLoginForm , TLoginForm
+from .models import students , teachers
 
 def roles(request):
     return render(request, 'login/roles.html')
 
 def Tlogin(request):
-    return render(request, 'login/teacher.html')
+    form = TLoginForm(request.POST or None)
+    error_message = None
+    if request.method == 'POST':
+        if form.is_valid():
+            code = form.cleaned_data['teacher_code'].strip()
+            password = form.cleaned_data['teacher_password'].strip()
+            if code == password:
+                try:
+                    teacher = teachers.objects.get(teacher_id=code)
+                    request.session['teacher_id'] = teacher.teacher_id
+                    return redirect('teacher_panel:panel')
+                except teachers.DoesNotExist:
+                    error_message = 'معلمی با این کد پرسنلی یافت نشد.'
+            else:
+                error_message = 'کد ملی و رمز عبور باید یکسان باشند.'
+
+    return render(request, 'login/teacher.html', {'form': form, 'error_message': error_message})
 
 def Slogin(request):
     form = SLoginForm(request.POST or None)
@@ -20,7 +36,7 @@ def Slogin(request):
                 try:
                     student = students.objects.get(student_id=code)
                     request.session['student_id'] = student.student_id
-                    return redirect('student_panel:panel')  # اینو با مسیر واقعی جایگزین کن
+                    return redirect('student_panel:panel')
                 except students.DoesNotExist:
                     error_message = 'دانش‌آموزی با این کد ملی یافت نشد.'
             else:
