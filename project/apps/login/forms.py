335034e82bdc612a9d3b3bd3e5e8f7e6fdadf8f6
VahideRamezani <vhiderm@gmail.com>
2025-06-03 19:14:50 +0330

    captcha debug

--- project/apps/login/forms.py
+++ project/apps/login/forms.py
@@ -6,16 +6,18 @@ class SLoginForm(forms.Form):
     st_code = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'w-full px-4 py-1 md:py-3 text-lg border border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white/90',
         'placeholder': 'کد ملی خود را به طور کامل وارد کنید'}))
-    st_password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'w-full px-4 py-1 md:py-3 text-lg border border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white/90',
-        'placeholder': 'رمز عبور شما همان کد ملی شما می باشد'}))
-    captcha = CaptchaField(widget=CaptchaTextInput(attrs={
-        'class': 'w-full sm:w-3/5 md:w-2/3 lg:w-3/4 xl:w-3/4 px-4 py-3 text-lg border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white/90 text-right',
-        'placeholder': 'کد امنیتی',
-        'style': 'direction: rtl; text-align: right;',
-        'direction': 'rtl',
-    }))
-
+    st_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'w-full px-4 py-3 text-lg border border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white/90',
+            'placeholder': 'رمز عبور شما همان کد ملی شما می باشد',
+        })
+    )
+    captcha = CaptchaField(
+        widget=CaptchaTextInput(attrs={
+            'class': 'w-full px-4 py-3 text-lg border border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white/90',
+            'placeholder': 'کد امنیتی را وارد کنید',
+        })
+    )
 
 class TLoginForm(forms.Form):
     teacher_code = forms.CharField(widget=forms.TextInput(attrs={
@@ -24,5 +26,8 @@ class TLoginForm(forms.Form):
     teacher_password = forms.CharField(widget=forms.PasswordInput(attrs={
         'class': 'col-span-9 px-4 py-2 md:py-3 text-lg border border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white/90',
         'placeholder': 'رمز عبور شما همان کد پرسنلی شما می باشد'}))
-    captcha = CaptchaField(widget=CaptchaTextInput(
-        attrs={}))
+    captcha = CaptchaField(widget=CaptchaTextInput(attrs={
+        'class': 'w-full px-4 py-3 text-lg border border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white/90',
+        'placeholder': 'کد امنیتی را وارد کنید',
+    }))
+
